18d052595af06dca23c784c9a9cc364d183eed9b
burmd1999 <dburmaster5@gmail.com>
2020-07-02 10:01:04 -0400

    hi

--- MI Estimators/miKSGmethod.m
+++ MI Estimators/miKSGmethod.m
@@ -1,28 +1,52 @@
 %GENERATE DATA
-m = 5000;
-data = zeros(m,2);
+m = 1000;
 
 %Creates random independent data
+data = zeros(m,2);
 for i = 1:m
-    data(i,1) = normrnd(0,4);
+    %data(i,1) = normrnd(0,4);
     %data(i,2) = normrnd(1,1);
-    %data(i,2) = f(data(i,1),muy,sdy);
-    %data(i,2) = data(i,1);
+    %data(i,2) = f(data(i,1),1,1);
+    
+    data(i,1) = rand;
+    data(i,2) = exp(-data(i,1));
 end
+
+%Expected value
+% EIxy = -0.5 - log(1-1/exp(1));
+EIxy = 1-3/exp(1)+2/exp(1)^2 - (1-1/exp(1))^2*log(1-1/exp(1));
+
+%creates bivariate random data
+% mux = 0;
+% muy = 0;
+% sdx = 1;
+% sdy = 1;
+% roh = 0.5;
+% covxy = roh*sdx*sdy;
+% mu = [mux muy];
+% sigma = [sdx^2 covxy;covxy sdy^2];
+% 
+% data = mvnrnd(mu,sigma,m);
+% %Expected value
+% EIxy = -0.5*log(1-roh^2) - roh^2/(2*(1-roh^2))*((mux^2+sdx^2)/sdx^2+(muy^2+sdy^2)/sdy^2) + roh^2/(1-roh^2);
+
+
 
 %MI CALCULATION GIVEN "DATA"
 %retrieve n-obs and d-dim
-[n,d] = size(data);
+[n,~] = size(data);
 
-k = 3;
+%k-number of nearest neighbours
+k = 2;
+%array for helping with the sum in MI formula
 array1 = zeros(n,1);
 
 for j = 1:n
-    %determines which point: p is the closest
+    %determines which points (vector p) are the closest
     dist = zeros(n,1);
     dist(:,1) = sqrt((data(:,1)-data(j,1)).^2 + (data(:,2)-data(j,2)).^2);
     dist(dist==0) = nan;
-    [mindist,p] = mink(dist,k);
+    [~,p] = mink(dist,k);
 
     %determines epsilon values of nearest neighbour
     ep = zeros(k,2);
@@ -47,7 +71,7 @@ for j = 1:n
 
     %count up ny
     ny = 0;
-    ub = data(j,2) + epy; %changes bounds for y
+    ub = data(j,2) + epy; %changes bounds for ny
     lb = data(j,2) - epy;
     for i = 1:n
         if i ~= j
@@ -73,4 +97,3 @@ function y = f(x, muy, sdy)
         y = normrnd(muy,sdy);
     end
 end
-
