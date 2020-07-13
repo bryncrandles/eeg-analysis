1235b2aef0201917d3ed773b62d2bb52dc34dbe0
burmd1999 <dburmaster5@gmail.com>
2020-07-13 19:18:19 -0400

    delete

--- Functions/migraph.m
+++
@@ -1,9 +0,0 @@
-function migraph(mi,ind,dep)
-    [~,~,t] = size(mi);
-    x = 0:t-1;
-    y = mi(ind,dep,:);
-
-    scatter(x,y,'filled');
-    ylabel('Mutual Information');
-    xlabel(['Dependence of var ' num2str(dep) ' on var ' num2str(ind) ' over time']);
-end
